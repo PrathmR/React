049f43aa26edd9991c28c3b6f8e8aa21f681ff08
Prathmesh Redekar <149141096+PrathmR@users.noreply.github.com>
2025-07-29 06:06:02 +0000

    forms using react

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setN] = useState('Prathmesh');
+
+  const [username, setUsername] = useState('')
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log(username);
+    setUsername(''); 
+  }
 
   return (
-    <>
     <div>
-      <h1>Username is {name}</h1>
-      <button onClick={()=> setN('Emperor')}> Change user</button>
-      {/* <h1 className="text-3xl font-bold underline line-through">Hello world! </h1> */}
+      <form onSubmit={(e)=>
+        submitHandler(e)
+      }>
+        <input value={username} 
+        onChange={(e)=>{
+          setUsername(e.target.value)
+        }}
+        className='px-4 rounded py-3 text-xl m-5 border-1 solid white font-semibold' type= 'text'
+        placeholder='Enter your name'></input>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded' type='submit'>Submit</button>
+      </form>
     </div>
-     
-    </>
-  )
+
+  )  
 }
 
 export default App
