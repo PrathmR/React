84473a095af8ec8f528dbd633f6ca397cda7f748
PrathmR <prathmeshredekar16@gmail.com>
2025-08-04 21:53:22 +0530

    wrapping App comp into browser dom

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter as BrouserRouter } from 'react-router-dom'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrouserRouter>
     <App />
-  </StrictMode>
+    </BrouserRouter>
+  
 )
