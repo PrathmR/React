fe6b73bcb40701a440399d2cd23a8408d9db69bc
PrathmR <prathmeshredekar16@gmail.com>
2025-08-04 22:06:40 +0530

    exporting the component

--- React-app/src/RoutersComp/Header.jsx
+++ React-app/src/RoutersComp/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 export const Header = () => {
   return (
-    <div className="py-7 px-10 bg-emrald-600 text-blac flex-center justify-between">
+    <div className="py-7 px-10 bg-white text-black flex-center justify-between">
       <h2 className="text-2xl">Prathmesh</h2>
       <div className="flex gap-10">
         <Link to="/">Home</Link>
