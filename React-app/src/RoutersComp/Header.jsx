a45445bb55235acaf8cbe1ac164067b134010287
PrathmR <prathmeshredekar16@gmail.com>
2025-08-06 20:14:26 +0530

    updating header comp

--- React-app/src/RoutersComp/Header.jsx
+++ React-app/src/RoutersComp/Header.jsx
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <div className="py-7 px-10 bg-white text-black flex-center justify-between">
       <h2 className="text-2xl">Prathmesh</h2>
+      <input type="text" className='bg-black text-white'/>
       <div className="flex gap-10">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
