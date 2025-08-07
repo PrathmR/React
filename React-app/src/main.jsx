1055e7a86d354fdd7d06936c5fbea3207b7d1d4f
PrathmR <prathmeshredekar16@gmail.com>
2025-08-07 19:33:22 +0530

    introducing imports for singleQuestion comp

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as BrouserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './AppSingleQ.css'
 createRoot(document.getElementById('root')).render(
   <BrouserRouter>
     <App />
