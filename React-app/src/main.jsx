df8f9236d44f3beb8785524f730f3ae9c2296191
PrathmR <prathmeshredekar16@gmail.com>
2025-08-12 20:38:51 +0530

    solving challenge5

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import { BrowserRouter as BrouserRouter } from 'react-router-dom'
 //import App from './App.jsx'
 import App from './TikTok UI/App.jsx';
