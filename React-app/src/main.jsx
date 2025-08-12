0603dc99efc0866085f59e5d40e8a15630de7ab1
PrathmR <prathmeshredekar16@gmail.com>
2025-08-12 20:07:14 +0530

    updating explanation of challenge4

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import { BrowserRouter as BrouserRouter } from 'react-router-dom'
 //import App from './App.jsx'
 import App from './TikTok UI/App.jsx';
