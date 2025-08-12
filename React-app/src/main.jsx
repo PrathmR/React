32e83b3020db69a09eafcda2402b4c3e67d3b5bb
PrathmR <prathmeshredekar16@gmail.com>
2025-08-12 19:28:33 +0530

    decolating app from tiktok to render

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as BrouserRouter } from 'react-router-dom'
-import App from './App.jsx'
+//import App from './App.jsx'
+import App from './TikTok UI/App.jsx';
 import './AppSingleQ.css'
 createRoot(document.getElementById('root')).render(
   <BrouserRouter>
