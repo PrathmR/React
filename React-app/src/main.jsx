78a244972b1676e727f9f6ebebd07caabd0b4047
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 18:42:18 +0530

    modifying main.jsx to render restaurant App.jsx

--- React-app/src/main.jsx
+++ React-app/src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as BrouserRouter } from 'react-router-dom'
 //import App from './App.jsx'
 //import App from './TikTok UI/App.jsx';
 //import './AppSingleQ.css'
-import App from './ToursApp/App.jsx';
+//import App from './ToursApp/App.jsx';
+import App from './Restaurant Menu App/App.jsx';
 createRoot(document.getElementById('root')).render(
   <BrouserRouter>
     <App />
