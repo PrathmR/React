861500db50945aa02df3c6400cc20e66046a77d2
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 19:56:56 +0530

    solving challenge3

--- React-app/src/Restaurant Menu App/Categories.jsx
+++ React-app/src/Restaurant Menu App/Categories.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = (props) => {
+    const filterItems=props.filterItems
+    return (
+        <div className="btn-container">
+            <button className="filter-btn">All</button>
+            <button className="filter-btn">Breakfast</button>
+            <button className="filter-btn"><Shakes></Shakes></button>
+        </div>
+      
+    );
 };
 
 export default Categories;
