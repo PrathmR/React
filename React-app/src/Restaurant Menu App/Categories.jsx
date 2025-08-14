356b963c24c20550e4f916abf9a0e25eb139fbd4
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 20:33:24 +0530

    solving challenge5

--- React-app/src/Restaurant Menu App/Categories.jsx
+++ React-app/src/Restaurant Menu App/Categories.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        All
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        Breakfast
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("shakes")}>
-        Shakes
-      </button>
+      {categories.map((category, index) => (
+        <button
+          key={index}
+          className="filter-btn"
+          onClick={() => filterItems(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 };
