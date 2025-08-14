6adbaa84604ead5a18150d7cc6fefdc6082dbb96
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 20:26:27 +0530

    solving challenge4

--- React-app/src/Restaurant Menu App/App.jsx
+++ React-app/src/Restaurant Menu App/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+import "./index.css"; // Assuming you have some styles in App.css
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
