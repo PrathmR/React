b6ef4bee578f6a627175ea610f7a86e841e2b191
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 18:45:36 +0530

    solving challenge1

---
+++ React-app/src/Restaurant Menu App/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  // make your changes here
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  );
+}
+
+export default App;
