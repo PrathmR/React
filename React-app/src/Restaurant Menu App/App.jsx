547b4974d1b8f5047a9f652b3d74fe35f7fb2aba
PrathmR <prathmeshredekar16@gmail.com>
2025-08-14 20:34:57 +0530

    adding challenge5

--- React-app/src/Restaurant Menu App/App.jsx
+++ React-app/src/Restaurant Menu App/App.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
-import "./index.css"; // Assuming you have some styles in App.css
+import "./index.css"; // Assuming you have some styles in index.css
 
 function App() {
+  const allCategories = ["all", ...new Set(items.map((item) => item.category))];
   const [menuItems, setMenuItems] = useState(items);
+  const [categories] = useState(allCategories);
 
-  // Filter based on category
   const filterItems = (category) => {
     if (category === "all") {
-      setMenuItems(items); // show all items
+      setMenuItems(items);
       return;
     }
     const newItems = items.filter((item) => item.category === category);
@@ -24,7 +25,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
